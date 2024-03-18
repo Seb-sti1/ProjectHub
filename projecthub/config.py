713d288f94db39e726d548997fe80013dffdf36b
Clément Laroudie <clem.laroudie@gmail.com>
2024-03-18 19:48:56 +0100

    pydantic config (#5)
    - Add settings loaded from .env file

---
+++ projecthub/config.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+from pathlib import Path
+#from getpass import getpass  # will be used to input tokens
+
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings
+
+__all__ = ["Settings", "Config"]
+
+DEFAULT_CONFIG_FILE = Path("~/.config/projecthub/core.json")
+
+
+class _Settings(BaseSettings):
+    """Automatically loads the settings from a `.env` file.
+
+    This is designed to be a Singleton instance. 
+    The settings are loaded once and can then be accessed using the Setting instance.
+    """
+
+    # The Path does not necessary exists, use FilePath otherwise
+    config_path: Path = Field(
+        default=DEFAULT_CONFIG_FILE,
+        description="The default path used to store the custom config file in a json format.",
+        examples=[ Path("~/.config/projecthub/core.json") ],
+    )
+
+Settings = _Settings()
+
+
+class _Config(BaseModel):
+    """Contains all the configurable fields in Project Hub.
+    They can be edited by creating a json file in the `config_path` location.
+
+    This is designed to be a Singleton instance. 
+    The configurations are loaded once and can then be accessed using the Config instance.
+    """
+
+    def __init__(self) -> None:
+        # TODO: load the configurations from the config file in kwargs if the file exists.
+        kwargs = {}
+        super().__init__(**kwargs)
+
+Config = _Config()
--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name='projecthub',
     version='0.0.2',
     python_requires='>=3.10',
-    install_requires=[],
+    install_requires=[
+        'pydantic',
+        'pdantic-settings'
+    ],
     entry_points={
         'console_scripts': [
             'projecthub = projecthub.main:main',
